d6b30d41bfe176a61f80a3f5d05b52cd982fe257
maxkenkomoda <tango12speedcell@gmail.com>
2022-01-06 21:21:24 +0900

    dev test headers

--- src/common/auth-guard.ts
+++ src/common/auth-guard.ts
@@ -11,7 +11,8 @@ export const AUTH_GUARD = function (
     const headers = req.headers
     const authorization = headers.authorization
     const apiUserInfo = headers['x-apigateway-api-userinfo']
-    console.log(headers)
+    console.log('api user info', apiUserInfo)
+    console.log('auth', authorization)
 
     if (!authorization || !apiUserInfo) {
       throw new Error('no headers')
